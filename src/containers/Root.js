ea8ae4c2c65dcc0c4a68aa7ed12594b803caa604
Chris Kihneman <ckihneman@gmail.com>
2017-05-29 01:55:50 -0700

    Use localStorage handlers to persist state

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
+import rootReducer from '../reducers';
+import { loadState, saveState } from '../localStorage';
 import configureStore from '../configureStore';
 import AppContainer from './AppContainer';
 
-const store = configureStore();
+const persistedState = loadState();
+const store = configureStore(rootReducer, persistedState);
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
 
 export default class Root extends Component {
     render() {
